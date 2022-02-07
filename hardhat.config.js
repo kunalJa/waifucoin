cf45b5bd324f596f498148f689e32be77d24b41b
kunalja <kunaljain@utexas.edu>
2022-02-06 22:31:50 -0500

    deploy to rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 require("@nomiclabs/hardhat-waffle");
 
 /**
@@ -5,4 +6,10 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.8.11",
+  networks: {
+    rinkeby: {
+      url: `${process.env.ALCHEMY_API_URL}`,
+      accounts: [`${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
